abf6b6dc6861000c70789ea09ccfbdf54e13b4b6
Alejandro Salas <asalas.wd@outlook.com>
2022-11-21 21:35:49 -0500

    Initial commit

---
+++ src/KeyChecker.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyChecker extends JFrame implements KeyListener {
+    Dimension dim = new Dimension(200, 200);
+    JLabel subtitle;
+
+
+
+    @Override
+    public void keyTyped(KeyEvent keyEvent) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent keyEvent) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent keyEvent) {
+
+    }
+}
